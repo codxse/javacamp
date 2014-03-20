05e68594a5d5f03b62faeb71aa477c34395b2711
nadiar429@gmail.com <nadiar429@gmail.com>
2014-03-20 16:42:50 +0700

    TestFormating has been added

--- src/pertemuan_1/TestFormating.java
+++ src/pertemuan_1/TestFormating.java
@@ -7,5 +7,15 @@ public class TestFormating {
 		System.out.println("i       : "+i);
 		System.out.printf("%%d      : %d%n", i);
 		System.out.printf("%%10d    : %10d%n", i);
+		System.out.printf("%%+10d   : %+10d%n", i);
+		System.out.printf("%%+10d   : %+10d%n", iminus);
+		System.out.printf("%%,10d   : %,10d%n", i);
+		System.out.printf("%%-10d   : %-10d%n", i);
+		double f=5675482.982;
+		System.out.println("f       : "+f);
+		System.out.printf("%%f      : %f%n", f);
+		System.out.printf("%%.2f    : %.2f%n", f);
+		System.out.printf("%%12.2f  : %12.2f%n", f);
+		System.out.printf("%%,12.2f : %,12.2f%n", f);
 	}
 }
