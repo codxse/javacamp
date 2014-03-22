6395d7b53a9b4a976af2b12788c2c1b3b9242e48
nadiar429@gmail.com <nadiar429@gmail.com>
2014-03-22 19:31:31 +0700

    operator logika

---
+++ src/pertemuan_1/OprLogika.java
@@ -0,0 +1,12 @@
+package pertemuan_1;
+
+public class OprLogika {
+	public static void main(String[] args) {
+		boolean a = true;
+		boolean b = false;
+		System.out.println("a && b = "+(a&&b));
+		System.out.println("a || b = "+(a||b));
+		System.out.println("!a = "+!a);
+		System.out.println("!(a && b) = "+!(a&&b));
+	}
+}
