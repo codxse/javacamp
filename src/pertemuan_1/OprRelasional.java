5ee9125f96ae2f9f13fdb587d7a03c39bc88d26c
nadiar429@gmail.com <nadiar429@gmail.com>
2014-03-22 09:55:07 +0700

    operator relasional

---
+++ src/pertemuan_1/OprRelasional.java
@@ -0,0 +1,14 @@
+package pertemuan_1;
+
+public class OprRelasional {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println("a == b: "+(a == b));
+		System.out.println("a != b: "+(a != b));
+		System.out.println("a > b: "+(a > b));
+		System.out.println("a < b: "+(a < b));
+		System.out.println("b >= a: "+(b >= a));
+		System.out.println("b <= a: "+(b <= a));
+	}
+}
